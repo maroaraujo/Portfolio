1730123a7e2e08c7fa22dcf1e33a97e91d2f75c6
Maria <maroaraujo@gmail.com>
2022-09-06 18:34:28 +0200

    added background for projects and css to scrollbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
-import Header from './Components/Header.js/Header';
-import world from "./img/world.png"
+import "./App.css";
+import Header from "./Components/Header.js/Header";
+import world from "./img/world.png";
 //import Image from './Components/Image/Image';
-import blur from "./img/blur.png"
-import planet from "./img/planet on top.png"
-import Welcome from './Components/Welcome/Welcome';
-import AboutMe from './Components/AboutMe/AboutMe';
-import Button from './Components/Button/Button';
+import blur from "./img/blur.png";
+import planet from "./img/planet on top.png";
+import AboutMe from "./Components/AboutMe/AboutMe";
+import Button from "./Components/Button/Button";
+import ProjectBG from "./img/Projects.png" 
 
 function App() {
   return (
-    <div className='site'>
-    <img className='planet' src={planet} alt={"planet"}/>
-    <img className='blur' src={blur} alt={"blur"}/>
-     {/* <Image className="planet" src={planet} alt={"planet"}/> */}
-     <Header/>
-    <div className="App">
-      {/* <Image className="world" src={world} alt={"myworld"}/> */}
-      {/* <Welcome className="welcome"/> */}
-      <img className="world" src={world} alt={"myworld"}/>
-    <AboutMe/>
-<Button className="cv"/>
-
-    </div>
+    <div className="site">
+      <img className="planet" src={planet} alt={"planet"} />
+      <img className="blur" src={blur} alt={"blur"} />
+      {/* <Image className="planet" src={planet} alt={"planet"}/> */}
+      <Header />
+      <div className="App">
+        {/* <Image className="world" src={world} alt={"myworld"}/> */}
+        {/* <Welcome className="welcome"/> */}
+        <img className="world" src={world} alt={"myworld"} />
+        <AboutMe />
+        <Button className="cv" />
+        <img src={ProjectBG} alt={"projects"}/>
+      </div>
     </div>
   );
 }
