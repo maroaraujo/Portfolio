22aee5bbd02cabc4ba7dcd4a4e058e046a744e29
Maria <maroaraujo@gmail.com>
2022-09-06 17:08:12 +0200

    added functionality to download button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Pdf from "../../documents/Maria Roberta CV test 2 pages.pdf";
 
 function Button(props) {
+
+  function onResumeClick() {window.open(Pdf)}
+  
   return (
-    <button className={props.className}>Download CV</button>
+ <div className='App'>
+     <button className='cv' onClick={onResumeClick}>Download CV</button> 
+     </div>
   )
 }
 
